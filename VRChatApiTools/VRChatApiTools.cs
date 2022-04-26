43359d4ba0b9afb91116ba701526c98e91f655ef
BocuD <duco.born@gmail.com>
2022-04-26 20:49:46 +0200

    Literally a single line cleanup

--- VRChatApiTools/VRChatApiTools.cs
+++ VRChatApiTools/VRChatApiTools.cs
@@ -73,7 +73,6 @@ namespace BocuD.VRChatApiTools
         {
             uploadedWorlds = null;
             uploadedAvatars = null;
-                        
 
             ImageCache.Clear();
             currentlyFetching.Clear();
